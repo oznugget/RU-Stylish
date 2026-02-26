3bac2c05e086781dbfa07e67964395b281ec5b46
Shumirai <shumigunzo@gmail.com>
2026-02-26 21:31:18 +0200

    tpying function for about

--- RU-/script.js
+++ RU-/script.js
@@ -7,3 +7,17 @@ function closeSidebar(){
     const navLinks = document.querySelector('.nav-links');
     navLinks.style.display = 'none';
 }
+
+//function for typing effect in about page
+//number 3 and 9s
+let varForTyping = 0;
+function typing(){
+    const line = "What is RU Stylish?";
+    
+    if(varForTyping < line.length){
+        document.getElementById("aboutSiteh2").innerHTML += line.charAt(varForTyping);
+        varForTyping++;
+        setTimeout(typing, 110);
+    }
+}
+window.onload = typing;
