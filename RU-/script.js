e13a3256b99b2fe09ca9a4cea25e324416fb1697
Chris Pretorius <chrispretorius295@gmail.com>
2026-02-26 19:06:47 +0200

    added sidebar to all pages. needs some design improvements but it works. also created a script.js file

---
+++ RU-/script.js
@@ -0,0 +1,9 @@
+function showSidebar(){
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.style.display = 'flex';
+}
+
+function closeSidebar(){
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.style.display = 'none';
+}
