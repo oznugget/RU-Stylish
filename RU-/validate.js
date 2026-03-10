f76d242480aaee44242e68ee126b738c24fdb9e8
Ntsumbedzeni Tshikovhi <kovhi.tshikovhi17.27@gmail.com>
2026-03-10 12:40:15 +0200

    modified terms page

---
+++ RU-/validate.js
@@ -0,0 +1,84 @@
+const form =document.getElementById('form')
+const LastName =document.getElementById('LastName')
+const FirstName =document.getElementById('FirstName')
+const Password =document.getElementById('Password')
+const Cpassword =document.getElementById('Cpassword')
+
+const ErrorMessages =document.getElementById('Err-Messages')
+
+
+const termsSection = document.getElementById("termsSection")
+form.addEventListener('submit',(e) =>{
+    let errors = []
+
+    //Check weather we are in signUp or Login page
+    if(FirstName){
+        errors=CreateAccErr(LastName.value,FirstName.value,Password.value,Cpassword.value)
+
+    
+    }else{
+        //errors=SigErr(username.value,Password.value)
+    }
+
+       if(errors.length > 0){
+        e.preventDefault()
+        ErrorMessages.innerText = errors.join(".")
+    }
+
+
+    //e.preventDefault()
+
+});
+
+
+
+function CreateAccErr(LastNameV,FirstNameV,PasswordV,CpasswordV){
+    let errors=[]
+
+    if(FirstNameV ==''||FirstNameV==null){
+        errors.push('Firstname is required')
+        FirstName.parentElement.classList.add('incorrect')
+    }
+     if(LastNameV ==''||LastNameV==null){
+        errors.push('LastName is required')
+        FirstName.parentElement.classList.add('incorrect')
+    }
+     if(PasswordV ==''||PasswordV==null){
+        errors.push('Password is required')
+        Password.parentElement.classList.add('incorrect')
+    }
+     if(CpasswordV ==''||CpasswordV==null){
+        errors.push('Confirm Password is required')
+        Cpassword.parentElement.classList.add('incorrect')
+    }
+     if(PasswordV !==CpasswordV){
+        errors.push('Firstname is required')
+        Cpassword.parentElement.classList.add('incorrect')
+        Password.parentElement.classList.add('incorrect')
+    }
+
+
+
+    return errors;
+}
+
+
+
+form.addEventListener('submit',(e) =>{
+    e.preventDefault()
+
+    let errors= CreateAccErr(
+        LastName.value,
+        FirstName.value,
+        Password.value,
+        Cpasssword.value
+    )
+
+    if(errors.length > 0){
+        ErrorMessages.innerText= errors.join(".")
+    }else{
+        form.style.display ="none",
+        termsSection.style.display ="block"
+    }
+})
+
