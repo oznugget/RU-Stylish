d46dc267ad03a8eab050686e2bed89bf9e685056
Chris Pretorius <chrispretorius295@gmail.com>
2026-03-12 01:05:30 +0200

    create listing only seen if logged in

--- RU-/Create_Acount.php
+++ RU-/Create_Acount.php
@@ -86,9 +86,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <ul class="nav-links">
                 <li><a href=# onclick="closeSidebar()"><img src="images/closeIcon.png" alt="Close Icon" width="30px" height="30px"></a></li>
                 <li><a href="index.php">Home</a></li>
-                <li><a href="listing.php">Create Listing</a></li>
+                
                 <?php if (isset($_SESSION['user_id'])): ?>
                     <li><a href="logout.php">Logout</a></li>
+                    <li><a href="listing.php">Create Listing</a></li>
                 <?php else: ?>
                     <li><a href="Create_Acount.php">Create Account</a></li>
                     <li><a href="SignIn.php">Sign in</a></li>
